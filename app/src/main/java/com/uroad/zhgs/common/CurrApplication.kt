0af0ebe68739c03b4f5b76b3aa7d6eefc511176f
mafeiben <mafeiben@u-road.com>
2018-10-09 10:17:03 +0800

    StatusBarUtils重命名

--- app/src/main/java/com/uroad/zhgs/common/CurrApplication.kt
+++ app/src/main/java/com/uroad/zhgs/common/CurrApplication.kt
@@ -1,35 +1,22 @@
 package com.uroad.zhgs.common
 
 import android.app.Activity
-import android.app.Application
-import android.content.Context
-import android.graphics.Typeface
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.support.multidex.MultiDex
 import android.text.TextUtils
 import android.util.Log
 import com.amap.api.maps.model.LatLng
-import com.liulishuo.filedownloader.model.FileDownloadStatus.paused
-import com.tencent.bugly.crashreport.CrashReport
 import com.uroad.library.utils.VersionUtils
 import com.uroad.library.utils.ZipUtils
 import com.uroad.rxhttp.RxHttpManager
 import com.uroad.rxhttp.RxHttpManager.addDisposable
 import com.uroad.rxhttp.download.DownloadListener
-import com.uroad.rxhttp.interceptor.Transformer
-import com.uroad.zhgs.R
 import com.uroad.zhgs.activity.VideoPlayerActivity
 import com.uroad.zhgs.model.DiagramMDL
-import com.uroad.zhgs.utils.AndroidBase64Utils
 import com.uroad.zhgs.utils.DiagramUtils
 import com.uroad.zhgs.utils.GsonUtils
-import com.uroad.zhgs.webservice.ApiService
 import com.uroad.zhgs.webservice.HttpRequestCallback
 import com.uroad.zhgs.webservice.WebApiService
-import java.io.File
-import java.nio.file.Files.exists
 
 /**
  *Created by MFB on 2018/7/26.
